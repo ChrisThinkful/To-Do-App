740767f2b62a044b04fb6a5bd6c8096854003a58
Chris <78775458+ChrisThinkful@users.noreply.github.com>
2021-11-23 20:54:57 -0600

    Adding components to the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 
+// components
+import Form from './Components/Form';
+import ToDoList from './Components/ToDoList';
+
 function App() {
   return (
     <div className="App">
       <header>
         Chris' To-Do List
       </header>
+      <Form />
+      <ToDoList />
     </div>
   );
 }
